a18015f489311a536a5777a5096dddef9c84a759
yazanalqasem <yalqasem@ipass-mena.com>
2024-05-09 18:46:21 +0530

    Update

--- Sources/Classes/DBDownloading.swift
+++ Sources/Classes/DBDownloading.swift
@@ -16,7 +16,13 @@ public class DataBaseDownloading{
     
     public static func deleet(){
         DocReader.shared.cancelDBUpdate()
-     
+        DocReader.shared.removeDatabase { (success, error) in
+            if success {
+                print(success) // Success state
+            } else {
+                print(error) // Error status
+            }
+        }
     }
     
     public static func initialization(completion: @escaping (String, String, String) -> Void) {
