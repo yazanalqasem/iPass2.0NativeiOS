a29a4dfa68329e6b78ae95edc16236e58b017180
yazanalqasem <yalqasem@ipass-mena.com>
2024-05-23 16:20:48 +0530

    Updated Code

--- Sources/Classes/DataManager.swift
+++ Sources/Classes/DataManager.swift
@@ -13,31 +13,31 @@ func getListOfFlows() -> [[String: Any]] {
     // Create dictionaries and add them to the array
     var dictionary1 = [String: Any]()
     dictionary1["flow"] = "Full processing"
-    dictionary1["flowId"] = 101
+    dictionary1["flowId"] = 10031
     dictionary1["Description"] = "This flow includes Document Scanning, Document Authenticity, User Liveness, User Face Matching, AML and social platform"
     arrayOfDictionaries.append(dictionary1)
 
     var dictionary2 = [String: Any]()
     dictionary2["flow"] = "IdVerification+liveness+AML"
-    dictionary2["flowId"] = 102
+    dictionary2["flowId"] = 10032
     dictionary2["Description"] = "This flow includes Document Scanning, Document Authenticity, User Liveness, User Face Matching and AML "
     arrayOfDictionaries.append(dictionary2)
     
     var dictionary3 = [String: Any]()
     dictionary3["flow"] = "Idverification+AML"
-    dictionary3["flowId"] = 103
+    dictionary3["flowId"] = 10015
     dictionary3["Description"] = "This flow includes Document Scanning, Document Authenticity and AML "
     arrayOfDictionaries.append(dictionary3)
     
     var dictionary4 = [String: Any]()
     dictionary4["flow"] = "idverification+liveness"
-    dictionary4["flowId"] = 104
+    dictionary4["flowId"] = 10011
     dictionary4["Description"] = "This flow includes Document Scanning, Document Authenticity, User Liveness and User Face Matching"
     arrayOfDictionaries.append(dictionary4)
     
     var dictionary5 = [String: Any]()
     dictionary5["flow"] = "IDV Only"
-    dictionary5["flowId"] = 105
+    dictionary5["flowId"] = 11111
     dictionary5["Description"] = "This flow includes Document Scanning and Document Authenticity"
     arrayOfDictionaries.append(dictionary5)
     
