68a82128eca62ae72097eb9b9bcb0167229f7bdc
yazanalqasem <yalqasem@ipass-mena.com>
2024-06-03 19:49:14 +0530

    Update

--- Sources/Classes/iPassManager.swift
+++ Sources/Classes/iPassManager.swift
@@ -651,38 +651,40 @@ public class iPassSDKManger {
     }
     
     private static func getIPAddress() -> String? {
-        var address: String?
-        var ifaddr: UnsafeMutablePointer<ifaddrs>?
+        var address : String?
 
-        // Get list of all interfaces on the device
+        // Get list of all interfaces on the local machine:
+        var ifaddr : UnsafeMutablePointer<ifaddrs>?
         guard getifaddrs(&ifaddr) == 0 else { return nil }
         guard let firstAddr = ifaddr else { return nil }
 
-        // Loop through linked list of interfaces
-        for ptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
-            let interface = ptr.pointee
+        // For each interface ...
+        for ifptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
+            let interface = ifptr.pointee
 
-            // Check for IPv4 or IPv6 interface
+            // Check for IPv4 or IPv6 interface:
             let addrFamily = interface.ifa_addr.pointee.sa_family
             if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+
+                // Check interface name:
+                // wifi = ["en0"]
+                // wired = ["en2", "en3", "en4"]
+                // cellular = ["pdp_ip0","pdp_ip1","pdp_ip2","pdp_ip3"]
                 
-                // Check interface name
                 let name = String(cString: interface.ifa_name)
-                if name == "en0" || name == "pdp_ip0" { // Wi-Fi or cellular interface
-                    
-                    // Convert interface address to a human-readable string
-                    var addr = interface.ifa_addr.pointee
-                    let addrLen = (addrFamily == UInt8(AF_INET)) ? INET_ADDRSTRLEN : INET6_ADDRSTRLEN
-                    var addrBuffer = [CChar](repeating: 0, count: Int(addrLen))
-                    if let addressPtr = inet_ntop(Int32(addrFamily), &addr, &addrBuffer, socklen_t(addrLen)) {
-                        address = String(cString: addressPtr)
-                        break
-                    }
+                if  name == "en0" || name == "en2" || name == "en3" || name == "en4" || name == "pdp_ip0" || name == "pdp_ip1" || name == "pdp_ip2" || name == "pdp_ip3" {
+
+                    // Convert interface address to a human readable string:
+                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    getnameinfo(interface.ifa_addr, socklen_t(interface.ifa_addr.pointee.sa_len),
+                                &hostname, socklen_t(hostname.count),
+                                nil, socklen_t(0), NI_NUMERICHOST)
+                    address = String(cString: hostname)
                 }
             }
         }
-
         freeifaddrs(ifaddr)
+
         return address
     }
     
