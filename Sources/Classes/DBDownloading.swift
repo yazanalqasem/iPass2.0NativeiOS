1098b38837673bfab3c34b3d4c1d3b90d95525ee
yazanalqasem <yalqasem@ipass-mena.com>
2024-07-09 11:47:49 +0530

    update

--- Sources/Classes/DBDownloading.swift
+++ Sources/Classes/DBDownloading.swift
@@ -29,18 +29,21 @@ public class DataBaseDownloading{
     
     public static func initialization(completion: @escaping (String, String, String) -> Void) {
         
+        print("print1111")
         do {
            // Amplify.Logging.logLevel = .verbose
+            print("print2222")
             try Amplify.add(plugin: AWSCognitoAuthPlugin())
             
             
            // try Amplify.configure()
         } catch {
+            print("print3333")
         }
         
         // Custom path to your configuration file
        
-        
+        print("print4444")
         
         let configFileName = "amplifyconfiguration.json"
 
@@ -49,17 +52,23 @@ public class DataBaseDownloading{
             if let amplifyConfig = loadAmplifyConfiguration(from: configFilePath) {
                 // Configure Amplify with the loaded configuration
                 do {
+                    print("print5555")
                     try Amplify.configure(amplifyConfig)
+                    print("print6666")
                 }
                 catch {
+                    print("print7777")
                 }
             } else {
+                print("print8888")
                 // Handle error loading or decoding configuration
             }
-        } else {
+        }
+        else {
+            print("print9999")
         }
         
-        
+        print("printAAAA")
         DocumentReaderService.shared.initializeDatabaseAndAPI(progress: { state in
             var progressValue = ""
             var status = ""
@@ -68,15 +77,19 @@ public class DataBaseDownloading{
             case .downloadingDatabase(progress: let progress):
                 let progressString = String(format: "%.1f", progress * 100)
                 progressValue = "Downloading database: \(progressString)%"
+                print("printBBBB")
             case .initializingAPI:
                 status = "Start Now"
+                print("printCCCC")
             case .completed:
                 break
             case .error(let text):
                 validationError = text
             }
+            print("printDDDD")
             completion(progressValue, status, validationError)
         })
+        print("printEEEE")
     }
     
     
