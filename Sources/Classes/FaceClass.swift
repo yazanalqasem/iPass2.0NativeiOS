3306c5c5a48f255e45bf14c45bc38283aa865a29
yazanalqasem <yalqasem@ipass-mena.com>
2024-07-09 18:47:09 +0530

    update

--- Sources/Classes/FaceClass.swift
+++ Sources/Classes/FaceClass.swift
@@ -1,6 +1,6 @@
 //
 //  SwiftUIView.swift
-//  
+//
 //
 //  Created by Vishal on 30/04/24.
 //
@@ -32,7 +32,7 @@ public struct FaceClass: View {
     var sessoinIdValue = ""
     var results: DocumentReaderResults!
     var isScanningTypeIndex:Int?
-
+    
     public var body: some View {
         
         
@@ -50,7 +50,7 @@ public struct FaceClass: View {
                     onCompletion: { result in
                         switch result {
                         case .success:
-                           
+                            
                             print("Success")
                             DispatchQueue.main.async {
                                 self.faceLivenessStringValue = "1" // Now you can modify this
@@ -76,14 +76,47 @@ public struct FaceClass: View {
                     }
                 ) .onAppear {
                     // Change the background color when the view appears
-                   print("PrintFFFF")
+                    print("PrintFFFF")
                 }
                 .sheet(isPresented: $isPresentingUserInfo) {
-
+                    
                 }
-
+                
+            }.onAppear(){
+                DispatchQueue.main.async {
+                    FaceClass.addAnimationLoader()
+                    
+                }
             }
         }
-       
-            }
+        
+    }
+    
+    private static func addAnimationLoader() {
+           var activityIndicator: UIActivityIndicatorView!
+        
+           let fullSizeView = UIView()
+           // Set background color
+        fullSizeView.backgroundColor = UIColor.black.withAlphaComponent(0.7)
+
+           // Add the view to the main view
+        iPassSDKDataManager.shared.controller.view.addSubview(fullSizeView)
+
+           // Constrain the view to the edges of the superview
+           fullSizeView.translatesAutoresizingMaskIntoConstraints = false
+           NSLayoutConstraint.activate([
+            fullSizeView.topAnchor.constraint(equalTo: iPassSDKDataManager.shared.controller.view.topAnchor),
+               fullSizeView.bottomAnchor.constraint(equalTo: iPassSDKDataManager.shared.controller.view.bottomAnchor),
+               fullSizeView.leadingAnchor.constraint(equalTo: iPassSDKDataManager.shared.controller.view.leadingAnchor),
+               fullSizeView.trailingAnchor.constraint(equalTo: iPassSDKDataManager.shared.controller.view.trailingAnchor)
+           ])
+        
+        
+        activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.color = iPassSDKDataManager.shared.loaderColor
+        activityIndicator.center =  iPassSDKDataManager.shared.controller.view.center
+        activityIndicator.hidesWhenStopped = true
+        fullSizeView.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+    }
 }
