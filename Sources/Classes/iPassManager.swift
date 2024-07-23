08c06187d18506367de754ec0f324879f28f9415
yazanalqasem <yalqasem@ipass-mena.com>
2024-07-23 17:19:35 +0530

    update

--- Sources/Classes/iPassManager.swift
+++ Sources/Classes/iPassManager.swift
@@ -210,12 +210,12 @@ public class iPassSDKManger {
     private static func checkUserPermission() {
         iPassHandler.methodForGetWithErrorMessages(urlStr: getPermissionStatus.baseApi + iPassSDKDataManager.shared.token + "&language=" + iPassSDKDataManager.shared.deviceCurrentLangauge ) { response, error in
             if(error != "") {
-               // DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
                   stopLoaderAnimation()
                     var tempDict = [String: String]()
                     tempDict = error?.convertToDictionary() ?? [:]
                     self.delegate?.getScanCompletionResult(result: "" , transactionId: "", error: tempDict["message"] ?? LocalizationManager.shared.localizedString(forKey: "limit_over"))
-               // }
+                }
                
             }
             else {
