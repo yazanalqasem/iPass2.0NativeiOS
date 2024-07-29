51c1e5c6786b4d3ebb7a2ea768ace2732717d37a
yazanalqasem <yalqasem@ipass-mena.com>
2024-07-29 12:27:56 +0530

    update

--- Sources/Classes/DBDownloading.swift
+++ Sources/Classes/DBDownloading.swift
@@ -13,18 +13,7 @@ import AWSCognitoAuthPlugin
 public class DataBaseDownloading{
     
     
-    public static func testDBIssue() {
-        DocReader.shared.runAutoUpdate(databaseID: "Full", progressHandler: { (progress) in
-            let progressString111 = String(format: "%.1f", progress.fractionCompleted * 100)
-            print(progressString111) // progress block
-        }, completion: { (success, error) in
-            if success {
-                print(success) // Success state
-            } else {
-                print(error) // Error status
-            }
-        })
-    }
+ 
     
     
     public static func initialization(completion: @escaping (String, String, String) -> Void) {
