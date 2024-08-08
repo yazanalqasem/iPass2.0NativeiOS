7c02eb530881aa9f61163d1e25da1bd7928c0913
yazanalqasem <yalqasem@ipass-mena.com>
2024-08-08 11:21:16 +0530

    update

--- Sources/Classes/DBDownloading.swift
+++ Sources/Classes/DBDownloading.swift
@@ -72,66 +72,66 @@ public class DataBaseDownloading{
     
     
     
-    public static func initializationww(completion: @escaping (String, String, String) -> Void) {
-        
-        var currentLanguage = "en"
-        if let preferredLanguageCode = Locale.preferredLanguages.first {
-             currentLanguage = Locale(identifier: preferredLanguageCode).languageCode ?? "en"
-            print("Device's preferred language code: \(currentLanguage)")
-          
-        } else {
-            print("Unable to determine the device's preferred language code.")
-        }
-        iPassSDKDataManager.shared.deviceCurrentLangauge = currentLanguage
-        
-        do {
-           // Amplify.Logging.logLevel = .verbose
-            try Amplify.add(plugin: AWSCognitoAuthPlugin())
-            
-            
-           // try Amplify.configure()
-        } catch {
-        }
-        
-        // Custom path to your configuration file
-       
-        
-        let configFileName = "amplifyconfiguration.json"
-
-        if let configFileURL = Bundle.module.url(forResource: configFileName, withExtension: nil) {
-            let configFilePath = configFileURL.path
-            if let amplifyConfig = loadAmplifyConfiguration(from: configFilePath) {
-                // Configure Amplify with the loaded configuration
-                do {
-                    try Amplify.configure(amplifyConfig)
-                }
-                catch {
-                }
-            } else {
-                // Handle error loading or decoding configuration
-            }
-        }
-        else {
-        }
-        
-        DocumentReaderService.shared.initializeDatabaseAndAPI(progress: { state in
-            var progressValue = ""
-            var status = ""
-            var validationError = ""
-            switch state {
-            case .downloadingDatabase(progress: let progress):
-                let progressString = String(format: "%.1f", progress * 100)
-                progressValue = "\(progressString)%"
-            case .initializingAPI:
-                status = "Start Now"
-            case .completed:
-                break
-            case .error(let text):
-                validationError = text
-            }
-            completion(progressValue, status, validationError)
-        })
-    }
+//    public static func initializationww(completion: @escaping (String, String, String) -> Void) {
+//        
+//        var currentLanguage = "en"
+//        if let preferredLanguageCode = Locale.preferredLanguages.first {
+//             currentLanguage = Locale(identifier: preferredLanguageCode).languageCode ?? "en"
+//            print("Device's preferred language code: \(currentLanguage)")
+//          
+//        } else {
+//            print("Unable to determine the device's preferred language code.")
+//        }
+//        iPassSDKDataManager.shared.deviceCurrentLangauge = currentLanguage
+//        
+//        do {
+//           // Amplify.Logging.logLevel = .verbose
+//            try Amplify.add(plugin: AWSCognitoAuthPlugin())
+//            
+//            
+//           // try Amplify.configure()
+//        } catch {
+//        }
+//        
+//        // Custom path to your configuration file
+//       
+//        
+//        let configFileName = "amplifyconfiguration.json"
+//
+//        if let configFileURL = Bundle.module.url(forResource: configFileName, withExtension: nil) {
+//            let configFilePath = configFileURL.path
+//            if let amplifyConfig = loadAmplifyConfiguration(from: configFilePath) {
+//                // Configure Amplify with the loaded configuration
+//                do {
+//                    try Amplify.configure(amplifyConfig)
+//                }
+//                catch {
+//                }
+//            } else {
+//                // Handle error loading or decoding configuration
+//            }
+//        }
+//        else {
+//        }
+//        
+//        DocumentReaderService.shared.initializeDatabaseAndAPI(progress: { state in
+//            var progressValue = ""
+//            var status = ""
+//            var validationError = ""
+//            switch state {
+//            case .downloadingDatabase(progress: let progress):
+//                let progressString = String(format: "%.1f", progress * 100)
+//                progressValue = "\(progressString)%"
+//            case .initializingAPI:
+//                status = "Start Now"
+//            case .completed:
+//                break
+//            case .error(let text):
+//                validationError = text
+//            }
+//            completion(progressValue, status, validationError)
+//        })
+//    }
     
     
     public static  func loadAmplifyConfiguration(from path: String) -> AmplifyConfiguration? {
