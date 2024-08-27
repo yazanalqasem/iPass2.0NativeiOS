7761ccdc6a99e66b33c0eb5a46915425b5b9ccdd
yazanalqasem <yalqasem@ipass-mena.com>
2024-08-27 15:59:12 +0530

    update

--- Sources/Classes/iPassManager.swift
+++ Sources/Classes/iPassManager.swift
@@ -730,7 +730,7 @@ public class iPassSDKManger {
                 SaveDataApi.workflow: String(iPassSDKDataManager.shared.userSelectedFlowId),
                 SaveDataApi.idv_data: documentDataJson ?? "",
                 SaveDataApi.language : iPassSDKDataManager.shared.deviceCurrentLangauge,
-                SaveDataApi.source: "iOS v1.0.4(Customdb)",
+                SaveDataApi.source: "iOS v1.0.4",
                 
             ]
             iPassHandler.methodForPost(url: SaveDataApi.baseApi + (iPassSDKDataManager.shared.token), params: parameters) { response, error in
