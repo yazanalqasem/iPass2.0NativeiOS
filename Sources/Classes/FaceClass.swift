b3d60f3ca0a7ed4f673ef6dc080ca8884e6b2773
yazanalqasem <yalqasem@ipass-mena.com>
2024-08-28 13:24:19 +0530

    update

--- Sources/Classes/FaceClass.swift
+++ Sources/Classes/FaceClass.swift
@@ -59,6 +59,7 @@ public struct FaceClass: View {
                             
                             print("Success")
                             DispatchQueue.main.async {
+                                iPassSDKDataManager.shared.alreadyReturned = false
                                 self.faceLivenessStringValue = "1" // Now you can modify this
                                 UserDefaults.standard.set(faceLivenessStringValue, forKey: "faceLiveness")
                                 self.isPresentingUserInfo = true
@@ -70,6 +71,7 @@ public struct FaceClass: View {
                         case .failure(_):
                             print("Failure")
                             DispatchQueue.main.async {
+                                iPassSDKDataManager.shared.alreadyReturned = false
                                 self.faceLivenessStringValue = "0" // Now you can modify this
                                 UserDefaults.standard.set(faceLivenessStringValue, forKey: "isFaceLiveness")
                                 self.isPresentingUserInfo = true
