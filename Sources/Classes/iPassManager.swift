1afb22aa5ea4f3fe022f77bf4771d02c9b45ad9d
yazanalqasem <yalqasem@ipass-mena.com>
2024-09-10 12:29:49 +0530

    Update

--- Sources/Classes/iPassManager.swift
+++ Sources/Classes/iPassManager.swift
@@ -405,7 +405,7 @@ public class iPassSDKManger {
         DocReader.shared.processParams.imageQA.brightnessThreshold = 1.5
 
         DocReader.shared.processParams.respectImageQuality = true
-        DocReader.shared.processParams.imageQA.dpiThreshold = 800
+        DocReader.shared.processParams.imageQA.dpiThreshold = 400
 
         DocReader.shared.processParams.minDPI = 400
         DocReader.shared.processParams.authenticityParams?.useLivenessCheck = NSNumber(value: iPassSDKDataManager.shared.needHologram)
