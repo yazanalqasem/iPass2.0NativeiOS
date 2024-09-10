9eaa5b2e684a00eca7a84237387c01a9ef1b2e4b
yazanalqasem <yalqasem@ipass-mena.com>
2024-09-10 13:18:39 +0530

    Update

--- Sources/Classes/iPassManager.swift
+++ Sources/Classes/iPassManager.swift
@@ -402,9 +402,9 @@ public class iPassSDKManger {
         DocReader.shared.processParams.imageQA.screenCapture = true
       //  DocReader.shared.processParams.imageQA.brightnessThreshold = true
         DocReader.shared.processParams.imageQA.documentPositionIndent = true
-        DocReader.shared.processParams.imageQA.brightnessThreshold = 1.5
+       // DocReader.shared.processParams.imageQA.brightnessThreshold = 1.5
 
-        DocReader.shared.processParams.respectImageQuality = true
+//        DocReader.shared.processParams.respectImageQuality = true
         DocReader.shared.processParams.imageQA.dpiThreshold = 400
 
        // DocReader.shared.processParams.minDPI = 400
