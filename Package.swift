ea8180deae427597fd1bb6f33865d4eab4f049e4
yazanalqasem <yalqasem@ipass-mena.com>
2025-03-25 12:19:53 +0530

    update

--- Package.swift
+++ Package.swift
@@ -18,9 +18,10 @@ let package = Package(
     ],
     dependencies: [
 //        .package(url: "https://github.com/cesarferreira/SwiftEventBus.git", from: "5.1.0"),
-        .package(url: "https://github.com/aws-amplify/amplify-ui-swift-liveness", from: "1.3.3")
+        .package(url: "https://github.com/aws-amplify/amplify-ui-swift-liveness", from: "1.3.3"),
+        .package(url: "https://github.com/FaciaMobile/ios-core.git", from: "3.1.3")
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        // .package(url: /* package url */, from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -28,7 +29,8 @@ let package = Package(
         .target(
             name: "iPass2.0NativeiOS",
            dependencies: ["DocumentReader", "DocumentReaderCore", "RegulaCommon" ,
-            .product(name: "FaceLiveness", package: "amplify-ui-swift-liveness")
+            .product(name: "FaceLiveness", package: "amplify-ui-swift-liveness"),
+            .product(name: "Facia", package: "ios-core"),
         ],
 
             path: "Sources",
