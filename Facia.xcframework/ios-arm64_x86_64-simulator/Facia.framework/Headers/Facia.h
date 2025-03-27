2cd1ae93d25db8bcf0824c042f236bcf8da952eb
yazanalqasem <yalqasem@ipass-mena.com>
2025-03-27 14:23:06 +0530

    update

---
+++ Facia.xcframework/ios-arm64_x86_64-simulator/Facia.framework/Headers/Facia.h
@@ -0,0 +1,18 @@
+//
+//  Facia.h
+//  Facia
+//
+//
+
+#import <Foundation/Foundation.h>
+#include<ifaddrs.h>
+
+//! Project version number for Facia.
+FOUNDATION_EXPORT double FaciaVersionNumber;
+
+//! Project version string for Facia.
+FOUNDATION_EXPORT const unsigned char FaciaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Facia/PublicHeader.h>
+
+
