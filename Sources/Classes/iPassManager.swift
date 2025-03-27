be9e294de1f67f4906c01b4a8d387559c9520575
yazanalqasem <yalqasem@ipass-mena.com>
2025-03-27 14:30:34 +0530

    update

--- Sources/Classes/iPassManager.swift
+++ Sources/Classes/iPassManager.swift
@@ -254,7 +254,7 @@ public class iPassSDKManger {
               "showResult" : true
           ]
         facia.createRequest(parentViewController: controllerReference,
-                            accessToken: "ACCESS_TOKEN",
+                            accessToken: "ZXlKcGRpSTZJbE5IV0dkcE4ycFRkWGxzTW10NGMzQjRZa2xHV0hjOVBTSXNJblpoYkhWbElqb2lORGd6ZFV0allraEljbTgzVVdwek1qUXJkVGRzT1VjNFVrdDZiWFJZWW1OTVVrcG5aRmxuUzAxdE9IWnFNa2x0TmtKc2VTOWxiekEwZG1ReFkzUnJPV2gzY1V0aFoyUk1jVzQ0UVU1UVZHWnBVVTlNV2k5aVRYWmpOWGh2ZEhKM05HWnBkVzVhY2sxRVNGaGtOSEp0WW1sUGNXZEZiVWhaZVVoNU4weDVURzlvUjNKeFNsSXlRVkExT0ZaM2FVSXZZM1ExTVRSTmNIaGtRakpoVkV4SFdGbHRWVkF3T1RCbWJVaFVaV1J0T0M4eFJYaG1VemxFZEdweWRGTkthRFYySWl3aWJXRmpJam9pWTJWbU5UUTRPVGswTmprd1lUaGhOVGN5TURrNFlqRTRaR1UwT1RabVltVXhZMlZtTVRKbE1ESTVORGxoTURVMk9HWXpNelk0WmpjMk1tVmpOemM0TXlJc0luUmhaeUk2SWlKOQ==",
                             configs: config) { result in
             print(result)
         }
