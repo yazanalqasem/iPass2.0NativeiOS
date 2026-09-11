f4e385ad80864db44b19b7925b6cb632fb7a84ea
mobileexpert1 <appsdev096@gmail.com>
2026-09-11 17:08:43 +0530

    updateVersion

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "iPass2.0NativeiOS",
     defaultLocalization: "en",
     platforms: [
-       .macOS(.v10_13), .iOS(.v14),
+       .macOS(.v10_13), .iOS(.v15),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
